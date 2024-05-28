45ce72e17aba3f83c503aca2dea858140d6b2485
tim.tang <tim72727752@gmail.com>
2024-05-29 01:13:21 +0800

    debug 6

--- src/models.py
+++ src/models.py
@@ -145,7 +145,7 @@ class OpenAIModel(ModelInterface):
             function_name = tool_call['function']['name']
             print(f"use ext function: {function_name}")
             function_to_call = self.available_functions[function_name]
-            # print(f'{tool_call=}')
+            print(f'{function_to_call=}')
             function_args = json.loads(tool_call['function']['arguments'])
             print(f'{function_args=}')
             function_response = function_to_call(query=function_args.get("query"))
