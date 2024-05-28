ade401287003192637a0f86af884a86b8691c66f
tim.tang <tim72727752@gmail.com>
2024-05-29 01:52:45 +0800

    add ext msg

--- api/main.py
+++ api/main.py
@@ -105,8 +105,8 @@ def handle_text_message(event):
                 raise Exception(error_message)
             tool_calls = get_tool_calls(response)
             if tool_calls:
-                # msg = TextMessage(text='處理中...')
-                # line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token, messages=[msg]))
+                waiting_msg = TextMessage(text='處理中請稍後...')
+                line_bot_api.reply_message(ReplyMessageRequest(reply_token=event.reply_token, messages=[waiting_msg]))
                 is_successful, response, error_message = user_model.chat_with_ext_second_response(memory.get(user_id), response,tool_calls,os.getenv('OPENAI_MODEL_ENGINE'))
                 role, response = get_role_and_content(response)
                 msg = TextMessage(text=response)
